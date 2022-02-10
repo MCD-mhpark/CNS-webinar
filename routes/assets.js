863423e77a05130e57f188f6e130da9dea92d319
MCD-mhpark <mhpark@goldenplanet.co.kr>
2022-02-10 19:42:07 +0900

    사전등록 api

--- routes/assets.js
+++ routes/assets.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 var fs = require('mz/fs');
 
 /**
- *  CDO의 id를 조회
+ *  CDO의 assets를 조회
  * 
  *  - params
  *      1. depth: 검색 수준을 지정. 
@@ -17,7 +17,7 @@ var fs = require('mz/fs');
  * 
  */
 
-router.get('/CDOid', function(req, res, next) {
+router.get('/CDO', function(req, res, next) {
 
     var searchString = "?name='" + req.query.name + "'";
 
@@ -34,6 +34,38 @@ router.get('/CDOid', function(req, res, next) {
         res.json(err);
     });
     
+});
+
+
+/**
+ *  Form의 assets를 조회
+ * 
+ *  - params
+ *      1. depth: 검색 수준을 지정. 
+ *          minimal - partial - complete 순으로 자세한 내용을 표시.
+ *          지정하지 않으면 minimal
+ *      2. name: 검색할 Form 이름
+ *          완전히 동일한 내용만 검색. 부분 검색을 허용하지 않음
+ * 
+ */
+
+router.get('/Form', function(req, res, next) {
+
+    var searchString = "?name='" + req.query.name + "'";
+
+    var queryString = {
+        depth: req.query.depth ? req.query.depth : 'minimal', 
+        search: searchString
+    }
+
+    cns_eloqua.assets.forms.get(queryString).then((result) => {
+        console.log(result.data);
+        res.json(result.data);
+    }).catch((err) => {
+        console.error(err.message);
+        res.json(err);
+    });
+ 
 });
 
 module.exports = router;
--- routes/webinar/preregist.js
+++ routes/webinar/preregist.js
@@ -6,6 +6,7 @@ var request_promise = require('request-promise');
 var moment = require('moment');
 var fs = require('mz/fs');
 const { route } = require('../assets');
+const { json } = require('express');
 
 /**
  *  로그인 API
@@ -13,6 +14,7 @@ const { route } = require('../assets');
  *  * body sample
  *    {
  *      "webinarName": "A", 
+ *      "webinarType": "Live",
  *      "hphone":"01011112222"
  *    }
  */
@@ -35,28 +37,38 @@ router.post('/login', async function (req, res, next) {
 
             var loginData = result.data.elements[0];
 
-            //2. 로그인 성공시 해당 CDO의 참석여부, 로그인시간 필드를 업데이트 ======== start
-            var loginUpdateData = loginData;
-            var loginUpdateDataFields = [
-                { type: 'FieldValue', id: '822', value: 'Y' },     // 참석여부 : Y/N (default:N)
-                { type: 'FieldValue', id: '807', value: moment().tz('Asia/Seoul').unix() }     // 로그인시간
-            ];
-            loginUpdateData.fieldValues = loginUpdateDataFields;
+            //2. 라이브 로그인 성공시 해당 CDO의 참석여부, 로그인시간 필드를 업데이트 ======== start
+            if (req.body.webinarType == 'Live') {
+                var loginUpdateData = loginData;
+                var loginUpdateDataFields = [
+                    { type: 'FieldValue', id: '822', value: 'Y' },     // 참석여부 : Y/N (default:N)
+                    { type: 'FieldValue', id: '807', value: moment().tz('Asia/Seoul').unix() }     // 로그인시간
+                ];
+                loginUpdateData.fieldValues = loginUpdateDataFields;
 
-            await cns_eloqua.data.customObjectData.update(parentId, loginData.id, loginUpdateData).then((result) => {
+                await cns_eloqua.data.customObjectData.update(parentId, loginData.id, loginUpdateData).then((result) => {
 
+                    console.log(loginData.uniqueCode + " : 업데이트");
+                    resultForm.uid = loginData.uniqueCode;
+                    resultForm.status = '1';
+
+                }).catch((err) => {
+
+                    console.log(err.message);
+
+                    //TODO: 갱신 실패에 대한 에러코드 협의
+                    resultForm.status = '-1';
+                })
+            }
+            //2. 라이브 로그인 성공시 해당 CDO의 참석여부, 로그인시간 필드를 업데이트 ======== end
+
+            //3. 다시보기 로그인 성공시 uid, status 전달 ======== start
+            else {
                 resultForm.uid = loginData.uniqueCode;
                 resultForm.status = '1';
+            }
+            //3. 다시보기 로그인 성공시 uid, status 전달 ======== end
 
-            }).catch((err) => {
-
-                console.log(err.message);
-
-                //TODO: 갱신 실패에 대한 에러코드 협의
-                resultForm.status = '-1';
-            })
-
-            //2. 로그인 성공시 해당 CDO의 참석여부, 로그인시간 필드를 업데이트 ======== end
         } else {
             resultForm.status = '0';
         }
@@ -74,123 +86,191 @@ router.post('/login', async function (req, res, next) {
 
 
 /**
- *  사전등록
+ *  사전등록 API
  * 
  */
-router.post('/preregist', function(req, res, next) {
+router.post('/preregist', async function(req, res, next) {
     
     var resultForm = {};
-
-    var hphone = req.body.hphone;
-    var formID = req.body.elqFormID;
-    var cdoID = req.body.elqCDOID;
-
+    
     // 1. 중복 데이터 조회 ===== start
-    var searchString = "?Mobile_Phone1='" + hphone + "'";
+    const cdoID = 80;
+    var searchString = "?______12='" + req.body.webinarName + "'___1='" + req.body.hphone + "'";
     var queryString = {
         'search' : searchString, 
         'depth' : 'complete'
     }
 
-    cns_eloqua.data.customObjectData.get(cdoID, queryString).then((result) => {
+    await cns_eloqua.data.customObjectData.get(cdoID, queryString).then((result) => {
         console.log(result.data);
 
+        //CDO 중복값 존재
         if (result.data.total > 0) {
             resultForm.status = "-1";
-            res.json(resultForm);
         }
 
     }).catch((err) => {
+        //통신에러
         console.log(err);
-        resultForm.status = "0";
-        res.json(resultForm);
+        resultForm.status = "-2";
     });
+
+    if (resultForm.hasOwnProperty('status')) {
+        return res.json(resultForm);
+    }
+
     // 1. 중복 데이터 조회 ===== end
 
     // 2. 폼 데이터 제출 ===== start
+    const formID = 383;
     var insertForm = mappedForm(req.body);
-    cns_eloqua.data.formData.create(formID, insertForm).then((result) => {
+    console.log(insertForm);
+    await cns_eloqua.data.formData.create(formID, insertForm).then(async (result) => {
+
+        console.log('form 데이터 제출 완료');
     
-        // 3. 제출 데이터 확인 ===== start
-        cns_eloqua.data.customObjectData.get(cdoID, queryString).then((result) => {
-            if (result.data.total > 0) {
+    }).catch((err) => {
+        //통신에러
+        console.log(err);
+        resultForm.status = "-2";
+    })
+
+    console.log('status 체크1');
+    if (resultForm.hasOwnProperty('status')) {
+        return res.json(resultForm);
+    }
+    // 2. 폼 데이터 제출 ===== end
+
+    // 3. 제출 데이터 확인 ===== start
+
+    // 엘로콰 통신 지연: 5초
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await cns_eloqua.data.customObjectData.get(cdoID, queryString).then(async (result) => {
+        console.log('제출 데이터 확인 결과값 받아옴   total = '+result.data.total);
+        if (result.data.total > 0) {    // CDO 제출 성공
+
+            // 4. Unique code copy 필드 업데이트 ===== start
+            var updateForm = result.data.elements[0];
+            updateForm.fieldValues = [
+                {
+                    "type": "FieldValue",
+                    "id": "838",
+                    "value": updateForm.uniqueCode
+                }
+            ];
+
+            await cns_eloqua.data.customObjectData.update(cdoID, updateForm.id, updateForm).then((result) => {
+                
+                // 업데이트 성공
                 resultForm.status = "1";
-                resultForm.uid = result.data.elements[0].uniqueCode;
-            } else {
-                resultForm.status = "0";
-                res.json(resultForm);
-            }
-            
-        }).catch((err) => {
+                resultForm.uid = updateForm.uniqueCode;
+                
+            }).catch((err) => {
+                //통신에러
+                console.log(err);
+                resultForm.status = "-2";
+            })
+            // 4. Unique code copy 필드 업데이트 ===== end
 
-
-        });
-        // 3. 제출 데이터 확인 ===== end
+        } else {    // CDO 제출 실패
+            resultForm.status = "0";
+        }
 
     }).catch((err) => {
-        
-    })
-    // 2. 폼 데이터 제출 ===== end
+        //통신에러
+        console.log(err);
+        resultForm.status = "-2";
+    });
+
+    // 3. 제출 데이터 확인 ===== end
+
+    // JSON 리턴
+    res.send(resultForm);
 });
 
 function mappedForm(data) {
 
-    var resultForm = {};
+    var resultform = {};
 
-    resultForm.type = "FormData";
-    resultForm.fieldValues = [
+    resultform.type = "FormData";
+    
+    resultform.fieldValues = [
         {
             "type": "FieldValue",
-            "id": "0",
-            "name": "lastname",
+            "id": "5820",
+            "name": "웨비나명",
+            "value": data.webinarName
+        },{
+            "type": "FieldValue",
+            "id": "5811",
+            "name": "Last Name",
             "value": data.lastname
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "name",
+            "id": "5812",
+            "name": "First Name",
             "value": data.name
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "email",
+            "id": "5810",
+            "name": "Email Address",
             "value": data.email
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "company",
+            "id": "5813",
+            "name": "Company",
             "value": data.company
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "title",
-            "value": data.title
-        },{
-            "type": "FieldValue",
-            "id": "0",
-            "name": "hphone",
+            "id": "5815",
+            "name": "Mobile Phone",
             "value": data.hphone
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "recom",
-            "value": data.recom
-        },{
-            "type": "FieldValue",
-            "id": "0",
-            "name": "agree1",
+            "id": "5821",
+            "name": "개인정보 수집•이용 동의 (필수)",
             "value": data.agree1
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "agree2",
+            "id": "5822",
+            "name": "개인정보 수집•이용 동의 (선택)",
             "value": data.agree2
         },{
             "type": "FieldValue",
-            "id": "0",
-            "name": "agree3",
+            "id": "5823",
+            "name": " 마케팅 정보 수신동의 (선택)",
             "value": data.agree3
+        },{
+            "type": "FieldValue",
+            "id": "5817",
+            "name": "ibch",
+            "value": data.ibch
+        },{
+            "type": "FieldValue",
+            "id": "5826",
+            "name": "폼 제출시간",
+            "value": moment().tz('Asia/Seoul').unix()
         }
     ];
+
+    //선택 필드
+    if (data.agree2 == 'Y') {
+        resultform.fieldValues.push({
+            "type": "FieldValue",
+            "id": "5824",
+            "name": "Title",
+            "value": data.title
+        });
+        resultform.fieldValues.push({
+            "type": "FieldValue",
+            "id": "5816",
+            "name": "추천인",
+            "value": data.recom
+        });
+    }
+
+    return resultform;
 }
 
 module.exports = router;
